da54d95f323f615a37bc079e44f7ba99f84d9c61
berkeks <berkekasabah@gmail.com>
2024-06-12 13:27:24 +0300

    Add functions to get and set current page in shared preferences

--- frontend/lib/Services/pageService.dart
+++ frontend/lib/Services/pageService.dart
@@ -2,6 +2,7 @@ import 'package:http/http.dart' as http;
 import 'package:language/Pages/OtherPages/error.dart';
 import 'package:language/Services/urls.dart';
 import 'package:language/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 getPage() async {
   var res = await http.get(Uri.parse(APIConfig.APIUrl));
@@ -10,4 +11,14 @@ getPage() async {
   } else {
     return const ErrorPage(500);
   }
+}
+
+getCurrentPage() async {
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  sharedPreferences.getString("currentPage");
+}
+
+void setCurrentPage(String value) async {
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  sharedPreferences.setString("currentPage", value);
 }
