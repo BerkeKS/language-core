8cd0ed9f86df5e345c7d4aae8252d457c462e801
berkeks <berkekasabah@gmail.com>
2024-06-13 22:09:57 +0300

    Update design of culture page

--- frontend/lib/Pages/Culture/Culture.dart
+++ frontend/lib/Pages/Culture/Culture.dart
@@ -1,145 +1,67 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:language/Pages/OtherPages/navBar.dart';
 
-class Culture extends StatefulWidget {
-  const Culture(this.iLink, this.iCLink, this.cultureExplore);
+class CulturePage extends StatefulWidget {
+  const CulturePage({required this.pageNumber});
 
-  final String iLink;
-  final String iCLink;
-  final List<CultureData> cultureExplore;
+  final int pageNumber;
 
   @override
-  State<Culture> createState() => _CultureState();
+  State<CulturePage> createState() => _CulturePageState();
 }
 
-class _CultureState extends State<Culture> {
-  int selectedCountry = 0;
-
-  dColor(int num) {
-    if (num == 0) {
-      return Color.fromARGB(255, 153, 0, 0);
-    } else if (num == 1) {
-      return Color.fromARGB(255, 0, 0, 0);
-    } else if (num == 2) {
-      return Color.fromARGB(255, 201, 57, 57);
-    } else if (num == 3) {
-      return Color.fromARGB(255, 202, 0, 0);
-    }
-  }
-
+class _CulturePageState extends State<CulturePage> {
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      scrollDirection: Axis.vertical,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RadioListTile(
-                value: 0,
-                groupValue: selectedCountry,
-                onChanged: (value) {
-                  setState(() {
-                    selectedCountry = 0;
-                  });
-                },
-                title: const Text("China")),
-            RadioListTile(
-                value: 1,
-                groupValue: selectedCountry,
-                onChanged: (value) {
-                  setState(() {
-                    selectedCountry = 1;
-                  });
-                },
-                title: const Text("Germany")),
-            RadioListTile(
-                value: 2,
-                groupValue: selectedCountry,
-                onChanged: (value) {
-                  setState(() {
-                    selectedCountry = 2;
-                  });
-                },
-                title: const Text("South Korea")),
-            RadioListTile(
-                value: 3,
-                groupValue: selectedCountry,
-                onChanged: (value) {
-                  setState(() {
-                    selectedCountry = 3;
-                  });
-                },
-                title: const Text("Spain")),
-          ],
-        ),
-        Stack(
-          children: [
-            Container(
-                height: 300,
-                width: 1000,
-                child: Image.asset(
-                  widget.iLink,
-                  fit: BoxFit.cover,
-                )),
-            Container(
-                alignment: Alignment.bottomLeft,
-                height: 70,
-                width: 110,
-                child: Image.asset(
-                  widget.iCLink,
-                  fit: BoxFit.cover,
-                ))
-          ],
-        ),
-        ListView(
-          children: [
-            ListView.builder(
-                itemCount: widget.cultureExplore.length,
-                itemBuilder: (context, index) {
-                  return ExpansionPanelList(
-                    dividerColor: dColor(selectedCountry),
-                    elevation: 2,
-                    expandedHeaderPadding: const EdgeInsets.all(6),
-                    expansionCallback: ((panelIndex, isExpanded) {
-                      setState(() {
-                        widget.cultureExplore[index].isOpen =
-                            !widget.cultureExplore[index].isOpen;
-                      });
-                    }),
-                    children: [
-                      ExpansionPanel(
-                          canTapOnHeader: true,
-                          headerBuilder: (context, isOpen) {
-                            return Padding(
-                              padding: EdgeInsets.all(4),
-                              child: Text(
-                                widget.cultureExplore[index].title,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            );
-                          },
-                          body: Padding(
-                            padding: EdgeInsets.all(6),
-                            child: Text(widget.cultureExplore[index].data),
-                          ),
-                          isExpanded: widget.cultureExplore[index].isOpen)
-                    ],
-                  );
-                })
-          ],
-        )
-      ],
+    return Scaffold(
+      drawer: NavBar(),
+      body: Builder(
+          builder: (context) => Row(
+                children: [
+                  IconButton(
+                    onPressed: () => Scaffold.of(context).openDrawer(),
+                    icon: const Icon(Icons.menu),
+                    color: Colors.black,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(color: Colors.blueGrey[600]),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.height / 100),
+                      child: Column(
+                        children: [
+                          Text("Title"),
+                          Row(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[350],
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: SizedBox(
+                                  child: Row(
+                                    children: [
+                                      Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          Image.asset(
+                                              "images/languages/Chinese.png"),
+                                          Text("China PR")
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )),
     );
   }
-}
-
-class CultureData {
-  bool isOpen = false;
-  String title;
-  String data;
-
-  CultureData(this.isOpen, this.title, this.data);
 }
