9e077a333c5f5ca4bb1e587d34302c347b5ab73b
berkeks <berkekasabah@gmail.com>
2024-07-02 12:31:04 +0300

    Fix location of menu button

--- frontend/lib/Pages/Culture/Culture.dart
+++ frontend/lib/Pages/Culture/Culture.dart
@@ -16,52 +16,139 @@ class _CulturePageState extends State<CulturePage> {
     return Scaffold(
       drawer: NavBar(),
       body: Builder(
-          builder: (context) => Row(
-                children: [
-                  IconButton(
-                    onPressed: () => Scaffold.of(context).openDrawer(),
-                    icon: const Icon(Icons.menu),
-                    color: Colors.black,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(color: Colors.blueGrey[600]),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: MediaQuery.of(context).size.height / 100),
-                      child: Column(
-                        children: [
-                          Text("Title"),
-                          Row(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.grey[350],
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: SizedBox(
-                                  child: Row(
-                                    children: [
-                                      Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceAround,
-                                        children: [
-                                          Image.asset(
-                                              "images/languages/Chinese.png"),
-                                          Text("China PR")
-                                        ],
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+          builder: (context) => Container(
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 70, 103, 101)),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                            onPressed: () => Scaffold.of(context).openDrawer(),
+                            icon: const Icon(Icons.menu),
+                            color: Colors.white)
+                      ],
+                    )
+                  ],
+                ),
               )),
     );
   }
 }
+
+/*
+  drawer: NavBar(),
+        body: Builder(
+            builder: (context) => Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  colors: [
+                    beginEnd[0],
+                    beginEnd[1],
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                )),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: () => Scaffold.of(context).openDrawer(),
+                            icon: const Icon(Icons.menu),
+                            color: MenuButtonColor(widget.unitNumber),
+                          ),
+                          Container(
+                            color: Colors.white,
+                            width: MediaQuery.of(context).size.width *
+                                (1300 / 1366),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                UnitSectionButton(
+                                  unithover: widget.unitNumber,
+                                  color: ButtonColor(widget.unitNumber),
+                                  text: "INTRODUCTION",
+                                  active: UnitStructure.unitPart == 0,
+                                  clicked: () {
+                                    setState(() {
+                                      UnitStructure.unitPart = 0;
+                                    });
+                                  },
+                                ),
+                                UnitSectionButton(
+                                  unithover: widget.unitNumber,
+                                  color: ButtonColor(widget.unitNumber),
+                                  text: "DIALOGUE",
+                                  active: UnitStructure.unitPart == 1,
+                                  clicked: () {
+                                    setState(() {
+                                      UnitStructure.unitPart = 1;
+                                    });
+                                  },
+                                ),
+                                UnitSectionButton(
+                                  unithover: widget.unitNumber,
+                                  color: ButtonColor(widget.unitNumber),
+                                  text: "VOCABULARY",
+                                  active: UnitStructure.unitPart == 2,
+                                  clicked: () {
+                                    setState(() {
+                                      UnitStructure.unitPart = 2;
+                                      UnitVocabulary.translation = '';
+                                      VocabularyCardState.selectedLanguage = "";
+                                      VocabularyCardState.dropDownGuide =
+                                          'Select Language';
+                                    });
+                                  },
+                                ),
+                                UnitSectionButton(
+                                  unithover: widget.unitNumber,
+                                  color: ButtonColor(widget.unitNumber),
+                                  text: "GRAMMAR",
+                                  active: UnitStructure.unitPart == 3,
+                                  clicked: () {
+                                    setState(() {
+                                      UnitStructure.unitPart = 3;
+                                    });
+                                  },
+                                ),
+                                UnitSectionButton(
+                                  unithover: widget.unitNumber,
+                                  color: ButtonColor(widget.unitNumber),
+                                  text: "EXERCISE",
+                                  active: UnitStructure.unitPart == 4,
+                                  clicked: () {
+                                    setState(() {
+                                      UnitStructure.unitPart = 4;
+                                    });
+                                  },
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          UnitContent(
+                            widget.unitNumber,
+                            widget.unitTitle,
+                            widget.unitVocabularyTopics,
+                            widget.unitGrammarTopics,
+                            widget.unitDialogue,
+                            widget.unitDialogueImage,
+                            widget.unitVocabularyTips,
+                            widget.unitGrammarData,
+                          ),
+                          const SizedBox(height: 5),
+                          UnitAccesser(widget.unitNumber, widget.unitBefore,
+                              widget.unitAfter),
+                        ],
+                      )
+                    ]))));
+*/
