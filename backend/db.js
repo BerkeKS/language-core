c54675dd522690954d1cf0e0995d60fd1f35a882
BerkeKS <berkekasabah@gmail.com>
2025-01-25 22:16:30 +0300

    Make strictQuery option false to fix deprecation error comes with Mongoose 7

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 async function conn(){
     try{
+        mongoose.set("strictQuery", false);
         await mongoose.connect(process.env.DB_CONN);
         console.log("Database is connected");
     }catch(err){
